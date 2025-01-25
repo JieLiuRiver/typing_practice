e8a30f2fef1c07b9c8fc143cd34f4e4ce8f2e3cb
Bode He <268079@163.com>
2025-01-25 14:31:06 +0800

    fix: bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import './App.css';
 import TypingEffect from './components/TypingEffect';
 import useTypingSound from './hooks/useTypingSound';
-import useWordSound from './hooks/useWordSound';
+import usePronunciationSound from './hooks/useWordSound';
+import { useEffect } from 'react';
 import { useAtom } from 'jotai';
 import { currentSentenceAtom, nextSentenceAtom } from './store';
 
 function App() {
   const { playSound } = useTypingSound();
-  const { playWord } = useWordSound();
   const [currentSentence] = useAtom(currentSentenceAtom);
   const [, nextSentence] = useAtom(nextSentenceAtom);
+  // 用于播放整个句子
+  const { play: playSentence } = usePronunciationSound(currentSentence?.source);
+
+  useEffect(() => {
+    // 在组件加载时播放整个句子
+    if (currentSentence?.source) {
+      playSentence();
+    }
+  }, [currentSentence?.source, playSentence]);
+
+  // const handleWordComplete = (word) => {
+  //   console.log('handleWordComplete',  word)
+  // };
 
   return (
     <>
@@ -17,13 +30,12 @@ function App() {
         <TypingEffect 
           text={currentSentence.source}
           onType={playSound}
-          onWordComplete={playWord}
           onComplete={() => {
             nextSentence()
           }}
         />
         <div className="translation">
-          翻译：{currentSentence.translation}
+          Translation: {currentSentence.translation}
         </div>
       </div>
     </>
