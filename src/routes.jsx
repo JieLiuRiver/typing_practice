577af5cc280d15e3ed5ad674e715eb77784ad190
Bode He <268079@163.com>
2025-01-26 14:48:29 +0800

    Updates router to use hash routing

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,8 +1,8 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import App from './App';
 import ConfigPage from './pages/ConfigPage';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <App />,
